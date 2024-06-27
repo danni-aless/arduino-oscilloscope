42b1fb54c3f2bf55a8428bd7f2c93529075b20f7
Alessandro D'Annibale <alessandro.dannibale02@gmail.com>
2024-06-27 19:24:23 +0200

    removed unused libraries

--- main_arduino.c
+++ main_arduino.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
--- uart.c
+++ uart.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdbool.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "uart.h"
